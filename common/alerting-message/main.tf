bc9f6c8278b056ce1684eeae29d7d62bb17ca6df
Tom Kaplonski <tomasz.kaplonski@sellics.com>
2022-03-22 08:30:30 +0100

    Add missing spaces

--- common/alerting-message/main.tf
+++ common/alerting-message/main.tf
@@ -1,15 +1,15 @@
 data "template_file" "alerting-message" {
   template = <<EOF
 $${prepend_text}
-{{#is_alert}} $${message_alert} {{/is_alert}}
-{{#is_alert_recovery}} $${message_alert}} {{/is_alert_recovery}}
-{{#is_alert_to_warning}} $${message_alert} $${message_warning} {{/is_alert_to_warning}}
+{{#is_alert}}  $${message_alert}  {{/is_alert}}
+{{#is_alert_recovery}}  $${message_alert}}  {{/is_alert_recovery}}
+{{#is_alert_to_warning}}  $${message_alert}  $${message_warning}  {{/is_alert_to_warning}}
 
-{{#is_warning}} $${message_warning} {{/is_warning}}
-{{#is_warning_recovery}} $${message_warning} {{/is_warning_recovery}}
+{{#is_warning}}  $${message_warning}  {{/is_warning}}
+{{#is_warning_recovery}}  $${message_warning}  {{/is_warning_recovery}}
 
-{{#is_no_data}} $${message_nodata} {{/is_no_data}}
-{{#is_no_data_recovery}} $${message_nodata} {{/is_no_data_recovery}}
+{{#is_no_data}}  $${message_nodata}  {{/is_no_data}}
+{{#is_no_data_recovery}}  $${message_nodata}  {{/is_no_data_recovery}}
 $${append_text}
 EOF
 
